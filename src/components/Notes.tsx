7d67fd20d9d06ce2abf52acf4d7e8f008b73b562
ziherMP-dev <mikedevstrong@gmail.com>
2024-11-27 20:30:20 +0100

    v6.1

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -255,6 +255,7 @@ export default function Notes() {
                   day: 'numeric',
                   hour: '2-digit',
                   minute: '2-digit',
+                  hour12: false
                 })}
               </p>
             </div>
